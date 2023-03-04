09232cd2ca70feff4ee62e3c245cf69f6d7140d3
Damien Rouzeau <86470640+DamienRouzeau@users.noreply.github.com>
2023-03-04 16:13:50 +0100

    interractions items

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -6,21 +6,22 @@ using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject scalpel, marteau;
     public Transform playerCam;
     public LayerMask interractableObject;
     RaycastHit hitinfo;
     bool handEmpty = true;
-
-
-
-
-
+    public GameObject scalpelInHand, marteauInHAnd;
+    bool haveScalpel, haveMarteau;
     bool canTakeItem;
+    GameObject itemInHand;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        scalpelInHand.SetActive(false);
+        marteauInHAnd.SetActive(false);
     }
 
     // Update is called once per frame
@@ -36,13 +37,39 @@ public class PlayerController : MonoBehaviour
             var ishit = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitinfo, 1000f, interractableObject);
             if (ishit)
             {
-                hitinfo.transform.position = new Vector3(0.3f, -0.2f, 0.3f);
+                
+                //hitinfo.transform.position = new Vector3(0.3f, -0.2f, 0.3f);
                 handEmpty= false;
+                if (hitinfo.collider.CompareTag("scalpel"))
+                {
+                    scalpelInHand.SetActive(true);
+                    haveScalpel= true;
+                    itemInHand = hitinfo.rigidbody.gameObject;
+                    itemInHand.SetActive(false);
+                }
+                else if (hitinfo.collider.CompareTag("marteau"))
+                {
+                    marteauInHAnd.SetActive(true);
+                    haveMarteau= true;
+                    itemInHand = hitinfo.rigidbody.gameObject;
+                    itemInHand.SetActive(false);
+                }
             }
         }
         else
         {
-            hitinfo.rigidbody.AddForce(Vector3.forward * 2f);
+            if (haveScalpel)
+            {
+                scalpelInHand.SetActive(false);
+                haveScalpel= false;
+            }
+            else if (haveMarteau)
+            {
+                marteauInHAnd.SetActive(false);
+                haveMarteau= false;
+            }
+            itemInHand.SetActive(true);
+            itemInHand.transform.position = transform.position;
             handEmpty = true;
         }
     }
